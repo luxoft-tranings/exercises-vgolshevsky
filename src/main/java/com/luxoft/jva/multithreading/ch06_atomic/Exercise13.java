8e62e2daf89f2d98a3cbb254dec2d841fcf62d1d
[Luxoft] Vladimir Olshevsky <VOlshevsky@luxoft.com>
2020-12-08 13:47:59 +0300

    Ex-13

--- src/main/java/com/luxoft/jva/multithreading/ch06_atomic/Exercise13.java
+++ src/main/java/com/luxoft/jva/multithreading/ch06_atomic/Exercise13.java
@@ -2,6 +2,8 @@ package com.luxoft.jva.multithreading.ch06_atomic;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static java.lang.System.out;
+
 /**
  * In this exercise we will play atomic ping-pong again:
  * <ul>
@@ -28,8 +30,84 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Exercise13 {
 
+	public static final int MAXHOP = 10_000;
 	public static void main(String[] args) {
-		// your code goes here
+		Exercise13.Ball ball = new Exercise13.Ball();
+
+		Thread ping = new Thread(new Exercise13.Ping(ball));
+		Thread pong = new Thread(new Exercise13.Pong(ball));
+
+		ping.setName("ping");
+		pong.setName("pong");
+
+		ping.start();
+		pong.start();
+		try
+		{
+			ping.join();
+			pong.join();
+		}
+		catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+
+		out.println("ping " + ball.ping + ", pong " + ball.pong);
+	}
+	static class Ball
+	{
+		//		public int ping = -1;
+		// 		public int pong = -1;
+		public AtomicInteger ping = new AtomicInteger(-1);
+		public AtomicInteger pong = new AtomicInteger(-1);
+	}
+
+	static class Ping implements Runnable
+	{
+
+		private final Exercise13.Ball ball;
+
+		Ping(Exercise13.Ball ball)
+		{
+			this.ball = ball;
+		}
+
+		@Override
+		public void run()
+		{
+			for (int i = 0; i < MAXHOP; i++)
+			{
+				ball.ping.set(i);
+				while (i != ball.pong.get())
+				{
+					// Thread.yield();
+				}
+			}
+		}
+	}
+
+	static class Pong implements Runnable
+	{
+
+		private final Exercise13.Ball ball;
+
+		Pong(Exercise13.Ball ball)
+		{
+			this.ball = ball;
+		}
+
+		@Override
+		public void run()
+		{
+			for (int i = 0; i < MAXHOP; i++)
+			{
+				while (ball.ping.get() != i)
+				{
+					// Thread.yield();
+				}
+				ball.pong.set(i);
+			}
+		}
 	}
 
 }
